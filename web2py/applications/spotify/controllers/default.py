df544616d96d5db3cdce4f4680aa4f2b31928a84
Will Retzloff <wretzloff@gmail.com>
2014-12-19 11:30:17 -0600

    Modifying text that flashes on Index page.

--- web2py/applications/spotify/controllers/default.py
+++ web2py/applications/spotify/controllers/default.py
@@ -10,14 +10,7 @@
 #########################################################################
 
 def index():
-    """
-    example action using the internationalization operator T and flash
-    rendered by views/default/index.html or views/generic.html
-
-    if you need a simple wiki simply replace the two lines below with:
-    return auth.wiki()
-    """
-    response.flash = T("Welcome to web2py!")
+    response.flash = T("Welcome to the Spotify app!")
     return dict(message=T('Hello World'))
 
 
