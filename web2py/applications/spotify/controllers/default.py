d735daf5c5ab4967e2c2cbade73784573e30e3fc
Will Retzloff <wretzloff@gmail.com>
2014-12-19 11:43:14 -0600

    Potential greeting message to be used later on the Index page

--- web2py/applications/spotify/controllers/default.py
+++ web2py/applications/spotify/controllers/default.py
@@ -11,6 +11,7 @@
 
 def index():
     response.flash = T("Welcome to the Spotify app!")
+    #response.title = "Logged in as: " + auth.user.first_name + "!"
     return dict(message=T('Hello World'))
 
 
