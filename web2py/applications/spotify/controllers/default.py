318137133a529ebf6ab83e9af2ff4c31a096bc01
Will Retzloff <wretzloff@gmail.com>
2014-12-20 18:44:01 -0600

    Requiring login to access Index page.

--- web2py/applications/spotify/controllers/default.py
+++ web2py/applications/spotify/controllers/default.py
@@ -1,14 +1,4 @@
-# -*- coding: utf-8 -*-
-# this file is released under public domain and you can use without limitations
-
-#########################################################################
-## This is a sample controller
-## - index is the default action of any application
-## - user is required for authentication and authorization
-## - download is for downloading files uploaded in the db (does streaming)
-## - api is an example of Hypermedia API support and access control
-#########################################################################
-
+@auth.requires_login()
 def index():
     response.flash = T("Welcome to the Spotify app!")
     #response.title = "Logged in as: " + auth.user.first_name + "!"
