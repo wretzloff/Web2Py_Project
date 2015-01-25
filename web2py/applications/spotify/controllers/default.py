b8eae1a153d1a56cd1674a35c3e4ef2a57704485
Will Retzloff <wretzloff@gmail.com>
2015-01-24 20:17:56 -0600

    Removing hardcoded HTTP parameters. Fetching parameters from config.

--- web2py/applications/spotify/controllers/default.py
+++ web2py/applications/spotify/controllers/default.py
@@ -2,23 +2,24 @@
 def index():
     import urllib2
     import urllib
-    response.flash = T("Welcome to the Spotify app!")
+    
     #Define URL
-    url = 'https://accounts.spotify.com/authorize'
+    url = getConfigValue('spotify_authorization_endpoint')
     #Define and encode parameters
     data = {}
-    data['client_id'] = '03281675a01a474ab8c9ecccc0646d82'
-    data['response_type'] = 'code'
-    data['redirect_uri'] = 'http://127.0.0.1:8000'
+    data['client_id'] = getConfigValue('spotify_client_id')
+    data['response_type'] = getConfigValue('spotify_response_type')
+    data['redirect_uri'] = getConfigValue('spotify_authorization_redirect_uri')
     url_values = urllib.urlencode(data)
     #Build full URL
     full_url = url + '?' + url_values
-    print full_url
+    #print full_url
     #Send GET request and print results
     #redirect(full_url)
     #data = urllib2.urlopen(full_url)
     #html = data.read()
     #print html
+    response.flash = T("Welcome to the Spotify app!")
     return dict(message=T('Hello World'), authenticate_url=full_url)
 
 
@@ -70,3 +71,10 @@ def api():
         '<tablename>': {'GET':{},'POST':{},'PUT':{},'DELETE':{}},
         }
     return Collection(db).process(request,response,rules)
+
+#Private function to fetch the config value specified by configValue
+def getConfigValue(configValue) :
+    redirectQueryResults = db(db.config.config_setting == 'spotify_authorization_redirect_uri').select()
+    redirectQueryFirstResult = redirectQueryResults[0]
+    redirect_uri = redirectQueryFirstResult.config_value
+    return redirect_uri
