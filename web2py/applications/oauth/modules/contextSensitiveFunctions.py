fd3eab0386a882bd75d6771b97cba9ef9df12dda
Will Retzloff <wretzloff@gmail.com>
2015-04-18 13:16:44 -0500

    Adding new module to hold functions that are context sensitive, meaning that they cannot be easily ported to a new Python project. Require access to db, session, etc.

---
+++ web2py/applications/oauth/modules/contextSensitiveFunctions.py
@@ -0,0 +1,54 @@
+import customFunctions
+
+#Private function to fetch the config value specified by configValue
+def getConfigValue(resourceOwner = None, configSetting = None, db = None) :
+    if resourceOwner is not None :
+        resourceOwnerConfigQueryResults = db(db.ResourceOwnerSettings.resourceOwnerName == resourceOwner).select()
+        resourceOwnerConfigFirstResult = resourceOwnerConfigQueryResults[0]
+        configValue = resourceOwnerConfigFirstResult[configSetting]
+        customFunctions.printToLog('getConfigValue: ' + configValue)
+    else:
+        configValueQueryResults = db(db.config.config_setting == configSetting).select()
+        configValueFirstResult = configValueQueryResults[0]
+        configValue = configValueFirstResult.config_value
+        customFunctions.printToLog('getConfigValue: ' + configValue)
+    return configValue
+
+def addOauthSessionVariable(session, oAuthVariableType, value, resourceOwner = None) :
+	customFunctions.printToLog('addOauthSessionVariable: oAuthVariableType: ' + str(oAuthVariableType))
+	customFunctions.printToLog('addOauthSessionVariable: resourceOwner: ' + str(resourceOwner))
+	customFunctions.printToLog('addOauthSessionVariable: value: ' + str(value))
+	if oAuthVariableType == 'access_token':
+		session.access_token = session.access_token or {}
+		session.access_token[resourceOwner] = value
+	elif oAuthVariableType == 'token_type':
+		session.token_type = session.token_type or {}
+		session.token_type[resourceOwner] = value
+	elif oAuthVariableType == 'expires_in':
+		session.expires_in = session.expires_in or {}
+		session.expires_in[resourceOwner] = value
+	elif oAuthVariableType == 'refresh_token':
+		session.refresh_token = session.refresh_token or {}
+		session.refresh_token[resourceOwner] = value	
+	else:
+		print 'error'
+
+def getOauthSessionVariable(session, oAuthVariableType, resourceOwner = None) :
+	customFunctions.printToLog('getOauthSessionVariable: oAuthVariableType: ' + str(oAuthVariableType))
+	customFunctions.printToLog('getOauthSessionVariable: resourceOwner: ' + str(resourceOwner))
+	if oAuthVariableType == 'access_token':
+		session.access_token = session.access_token or {}
+		returnValue = session.access_token[resourceOwner]
+	elif oAuthVariableType == 'token_type':
+		session.token_type = session.token_type or {}
+		returnValue = session.token_type[resourceOwner]
+	elif oAuthVariableType == 'expires_in':
+		session.expires_in = session.expires_in or {}
+		returnValue = session.expires_in[resourceOwner]
+	elif oAuthVariableType == 'refresh_token':
+		session.refresh_token = session.refresh_token or {}
+		returnValue = session.refresh_token[resourceOwner]
+	else:
+		returnValue = None
+	customFunctions.printToLog('getOauthSessionVariable: returnValue: ' + str(returnValue))
+	return returnValue
