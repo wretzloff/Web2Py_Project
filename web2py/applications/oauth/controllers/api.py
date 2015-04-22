7ab3e94efd9614b685f73937bb8b971702590e65
Will Retzloff <wretzloff@gmail.com>
2015-04-22 16:01:38 -0500

    Experimenting with API to expose business logic.

--- web2py/applications/oauth/controllers/api.py
+++ web2py/applications/oauth/controllers/api.py
@@ -1,20 +1,11 @@
 import oauthFunctions
 
 @request.restful()
-def testEndpoint():
+def buildUrlToInitiateAuthorization():
     def GET():
-        return 'returnStringFromTestEndpointGET'
+        return 'returnStringFrombuildUrlToInitiateAuthorizationGET'
     def POST(*args,**vars):
-        #message = 'DEFAULT'
-        #target = 'TARGET'
-        #if len(args) > 0:
-        #    if 'send' == args[0]:
-        #        message = args[1]
-        #        target = args[2]
-        #elif vars['type'] == 'send':
-        #    message = vars['message']
-        #    target = vars['to']
-        return 'returnStringFromTestEndpointPOST'
+        return 'returnStringFrombuildUrlToInitiateAuthorizationPOST'
     def PUT(*args,**vars):
         return ''
     def DELETE():
