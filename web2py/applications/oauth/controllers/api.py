16f92a063809ce5880b6a4c469130247cbe71590
Will Retzloff <wretzloff@gmail.com>
2015-04-22 15:46:38 -0500

    Experimenting with API to expose business logic. Test endpoint can be reached at http://127.0.0.1:8000/oauth/api/testEndpoint/

--- web2py/applications/oauth/controllers/api.py
+++ web2py/applications/oauth/controllers/api.py
@@ -1,3 +1,22 @@
 import oauthFunctions
-def buildUrlToInitiateAuthorization():
-    return 'abc'
+
+@request.restful()
+def testEndpoint():
+    def GET():
+        return 'returnStringFromTestEndpointGET'
+    def POST(*args,**vars):
+        #message = 'DEFAULT'
+        #target = 'TARGET'
+        #if len(args) > 0:
+        #    if 'send' == args[0]:
+        #        message = args[1]
+        #        target = args[2]
+        #elif vars['type'] == 'send':
+        #    message = vars['message']
+        #    target = vars['to']
+        return 'returnStringFromTestEndpointPOST'
+    def PUT(*args,**vars):
+        return ''
+    def DELETE():
+        return ''
+    return dict(GET=GET, POST=POST, PUT=PUT, DELETE=DELETE)
