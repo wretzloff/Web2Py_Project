1370ff69fe8cc823d95d3f6ee9c4aa4aa190b042
Will Retzloff <wretzloff@gmail.com>
2015-04-26 19:23:58 -0500

    Update postToTokenEndpointAuthorizationCodeSpotify() to get the URL through an API call instead of a call to a function.

--- web2py/applications/oauth/controllers/default.py
+++ web2py/applications/oauth/controllers/default.py
@@ -38,7 +38,6 @@ def buildUrlToInitiateAuthorizationSpotify() :
     redirect_uri = contextSensitiveFunctions.getConfigValue(None,'oAuthRedirectUri',db)
     scope = contextSensitiveFunctions.getConfigValue('Spotify','scopes',db)
     show_dialog = contextSensitiveFunctions.getConfigValue('Spotify','show_dialog',db)
-    
     #Call the API endpoint to generate a return a URL
     apiEndpoint = 'http://' + socket.gethostbyname(socket.gethostname()) + ':8000' + URL(None,'api','buildUrlToInitiateAuthorization')
     parameterArray = {'authorization_endpoint' : url,
@@ -49,7 +48,6 @@ def buildUrlToInitiateAuthorizationSpotify() :
                       'show_dialog' : show_dialog}
     apiURL = httpFunctions.buildFullUrl(apiEndpoint, parameterArray)
     full_url = httpFunctions.getRequest(apiURL)
-    
     return full_url
 
 #Helper function to send an HTTP POST request to the /token endpoint using an OAuth Authorization Code
@@ -59,7 +57,6 @@ def postToTokenEndpointAuthorizationCodeSpotify(codeParameterForPostRequest) :
     redirect_uri = contextSensitiveFunctions.getConfigValue(None,'oAuthRedirectUri',db)
     client_id = contextSensitiveFunctions.getConfigValue('Spotify','client_id',db)
     client_secret = contextSensitiveFunctions.getConfigValue('Spotify','client_secret',db)
-    ###################################################################################
     #Call the API endpoint to send an HTTP POST request and return the response data to us.
     apiEndpoint = 'http://' + socket.gethostbyname(socket.gethostname()) + ':8000' + URL(None,'api','postToTokenEndpointAuthorizationCode')
     parameterArray = {'postUrl' : postUrl,
@@ -67,14 +64,11 @@ def postToTokenEndpointAuthorizationCodeSpotify(codeParameterForPostRequest) :
                       'oAuthRedirectUri' : redirect_uri,
                       'client_id' : client_id,
                       'client_secret' : client_secret}
-    #apiURL = httpFunctions.buildFullUrl(apiEndpoint, parameterArray)
-    #print 'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb' + apiURL
-    #responseDataInJson = httpFunctions.getRequest(apiURL)
-    #print 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa' + responseDataInJson
-    #responseDataInArray2 = httpFunctions.convertJsonToArray(responseDataInJson)
-    #print 'cccccccccccccccccccccccccccccccccccccccccccccccccccccc' + responseDataInArray['access_token']
-    ###################################################################################
-    responseDataInArray = oauthFunctions.postToTokenEndpointAuthorizationCode(postUrl, codeParameterForPostRequest, redirect_uri, client_id, client_secret)
+    apiURL = httpFunctions.buildFullUrl(apiEndpoint, parameterArray)
+    responseDataInJson = httpFunctions.getRequest(apiURL)
+    #Convert JSON string to an array
+    responseDataInArray = httpFunctions.convertJsonToArray(responseDataInJson)
+    #Store data to session
     contextSensitiveFunctions.addOauthSessionVariable(session, 'access_token', responseDataInArray['access_token'], 'spotify')
     contextSensitiveFunctions.addOauthSessionVariable(session, 'token_type', responseDataInArray['token_type'], 'spotify')
     contextSensitiveFunctions.addOauthSessionVariable(session, 'expires_in', responseDataInArray['expires_in'], 'spotify')
