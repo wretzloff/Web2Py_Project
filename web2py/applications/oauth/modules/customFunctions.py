419f337498dde8411fdeff06309b1fca8a7bb51f
Will Retzloff <wretzloff@gmail.com>
2015-05-05 16:36:48 -0500

    Add ability to specify to add a newline to the log.

--- web2py/applications/oauth/modules/customFunctions.py
+++ web2py/applications/oauth/modules/customFunctions.py
@@ -2,5 +2,7 @@ def getTimestamp() :
     import time
     return time.strftime("%d/%m/%Y") + ' ' + time.strftime("%H:%M:%S")
 
-def printToLog(stringToPrint) :
+def printToLog(stringToPrint, newline = 0) :
+    if newline:
+        print '\n'
     print getTimestamp() + '\t' + stringToPrint
