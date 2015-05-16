f75359b5567a8c25c3ef58497f99b09459131e11
Will Retzloff <wretzloff@gmail.com>
2015-05-15 19:07:16 -0500

    Starting point for Spotify-specific API

---
+++ web2py/applications/oauth/controllers/apiSpotify.py
@@ -0,0 +1,14 @@
+import customFunctions
+import httpFunctions
+
+@request.restful()
+def testEndpoint():
+    def GET():
+        return 'abc!'
+    def POST(*args,**vars):
+        return ''
+    def PUT(*args,**vars):
+        return ''
+    def DELETE():
+        return ''
+    return dict(GET=GET, POST=POST, PUT=PUT, DELETE=DELETE)
