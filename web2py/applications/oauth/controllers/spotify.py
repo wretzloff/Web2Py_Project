50740618f61207395ad9ac7fc804047ebc86b728
Will Retzloff <wretzloff@gmail.com>
2015-05-21 00:48:03 -0500

    Incrementally moving logic from controller to Spotify-specific API.

--- web2py/applications/oauth/controllers/spotify.py
+++ web2py/applications/oauth/controllers/spotify.py
@@ -5,6 +5,10 @@ import socket
 def index():
     customFunctions.printToLog('------------------------------------------------', 0)
     customFunctions.printToLog('landingPageSpotify()', 1)
+    #########################
+    #Experimental section. Moving logic out of controller and into the Spotify-specific API.
+    callMeEndpoint()
+    #########################
     
     #Define the path to the API endpoint to fetch a configuration value
     configValueApiEndpoint = 'http://' + socket.gethostbyname(socket.gethostname()) + ':8000' + URL(None,'api','getConfigValue')
@@ -21,3 +25,13 @@ def index():
     responseDataInArray = httpFunctions.convertJsonToArray(responseDataInJson)
     authorizedUserEmailAddress = responseDataInArray['email']
     return dict(message='Authenticated with Spotify as: ' + authorizedUserEmailAddress)
+
+
+#Helper function......................
+def callMeEndpoint() :
+    apiEndpoint = contextSensitiveFunctions.getApiEndpoint('testEndpoint', 'Spotify')
+    #parameterArray = {'resourceOwner' : resourceOwner,
+    #                  'oAuthRedirectUri' : redirect_uri}
+    #apiURL = httpFunctions.buildFullUrl(apiEndpoint, parameterArray)
+    #full_url = httpFunctions.getRequest(apiURL)
+    #return full_url
