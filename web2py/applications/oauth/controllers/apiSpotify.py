cf7d7285283685b7204e24babf9f955610ffbd53
Will Retzloff <wretzloff@gmail.com>
2015-05-21 00:50:24 -0500

    Incrementally moving logic from controller to Spotify-specific API.

--- web2py/applications/oauth/controllers/apiSpotify.py
+++ web2py/applications/oauth/controllers/apiSpotify.py
@@ -2,6 +2,18 @@ import customFunctions
 import httpFunctions
 
 @request.restful()
+def callMeEndpoint():
+    def GET():
+        return 'callMeEndpoint!'
+    def POST(*args,**vars):
+        return ''
+    def PUT(*args,**vars):
+        return ''
+    def DELETE():
+        return ''
+    return dict(GET=GET, POST=POST, PUT=PUT, DELETE=DELETE)
+
+@request.restful()
 def testEndpoint():
     def GET():
         return 'abc!'
