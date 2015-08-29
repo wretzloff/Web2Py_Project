f648e962150624d6765420474807354d989a6404
Will Retzloff <wretzloff@gmail.com>
2015-08-29 16:55:46 -0500

    Comments

--- web2py/applications/oauth/controllers/apiSpotify.py
+++ web2py/applications/oauth/controllers/apiSpotify.py
@@ -4,6 +4,11 @@ import httpFunctions
 @request.restful()
 def callMeEndpoint():
     def GET():
+        #1. Need to retrieve the URL of the Spotify's Me Endpoint.
+            #1a. This is currenty only accessible through the getConfigValue API endpoint.
+            #ab. Need to move that functionality into a module, so that it can be accessed from the getConfigValue API endpoint, as well as from here.
+        #2. Send message to endpoint, incouding authorization information.
+        #3. Take the response from th endpoint and return it to the caller.
         return 'callMeEndpoint!'
     def POST(*args,**vars):
         return ''
