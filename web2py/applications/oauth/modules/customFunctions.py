e2be51e0601e0ed7e38b6910560386cad4d5e62e
Will Retzloff <wretzloff@gmail.com>
2015-09-13 14:51:59 -0500

    Experimenting with logging

--- web2py/applications/oauth/modules/customFunctions.py
+++ web2py/applications/oauth/modules/customFunctions.py
@@ -1,3 +1,4 @@
+import logging
 def getTimestamp() :
     import time
     return time.strftime("%d/%m/%Y") + ' ' + time.strftime("%H:%M:%S")
@@ -6,3 +7,10 @@ def printToLog(stringToPrint, newline = 0) :
     if newline:
         print '\n'
     print getTimestamp() + '\t' + stringToPrint
+	#logger = logging.getLogger('myapp')
+	#hdlr = logging.FileHandler('C:\Users\Will\Desktop\myapp.log')
+	#formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+	#hdlr.setFormatter(formatter)
+	#logger.addHandler(hdlr) 
+	#logger.setLevel(logging.DEBUG)
+	#logger.debug(stringToPrint)
