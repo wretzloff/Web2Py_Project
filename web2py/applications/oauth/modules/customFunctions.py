45f03788e6c3998e4cfccac37b14f77575a0b5e6
Will Retzloff <wretzloff@gmail.com>
2015-09-19 20:22:02 -0500

    Experimenting with logging.

--- web2py/applications/oauth/modules/customFunctions.py
+++ web2py/applications/oauth/modules/customFunctions.py
@@ -8,10 +8,7 @@ def printToLog(stringToPrint, newline = 0) :
 	if newline:
 		print '\n'
 	print getTimestamp() + '\t' + stringToPrint
-	#logger = logging.getLogger('log')
-	#hdlr = logging.FileHandler(os.getcwd() + '\logs\log.txt')
-	#formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
-	#hdlr.setFormatter(formatter)
-	#logger.addHandler(hdlr) 
-	#logger.setLevel(logging.DEBUG)
-	#logger.debug(stringToPrint)
+	#
+	logger = logging.getLogger("web2py.app.app")
+	logger.setLevel(logging.DEBUG)
+	logger.debug(stringToPrint)
