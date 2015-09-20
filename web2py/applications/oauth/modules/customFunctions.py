2a7afaa94dccaeb4c76300d9d37b97a8c5d6d069
Will Retzloff <wretzloff@gmail.com>
2015-09-19 20:27:24 -0500

    Comments

--- web2py/applications/oauth/modules/customFunctions.py
+++ web2py/applications/oauth/modules/customFunctions.py
@@ -5,10 +5,13 @@ def getTimestamp() :
     return time.strftime("%d/%m/%Y") + ' ' + time.strftime("%H:%M:%S")
 
 def printToLog(stringToPrint, newline = 0) :
+	#Print to console
 	if newline:
 		print '\n'
 	print getTimestamp() + '\t' + stringToPrint
 	#
+	#Print to file
 	logger = logging.getLogger("web2py.app.app")
 	logger.setLevel(logging.DEBUG)
 	logger.debug(stringToPrint)
+	
